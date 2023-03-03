b90e40090085c5f717e30f9e5c304f73faf67ab8
James Alhassan <jamesalhassan052@gmail.com>
2023-03-03 20:52:25 +0000

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -25,3 +25,22 @@ size_t binary_tree_height_b(const binary_tree_t *tree)
 		return ((l > r) ? l : r);
 	}
 }
+
+/**
+ * binary_tree_balance - Measures balance factor of a binary tree
+ * @tree: tree to go through
+ * Return: balanced factor
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int right = 0, left = 0, total = 0;
+
+	if (tree)
+	{
+		left = ((int)binary_tree_height_b(tree->left));
+		right = ((int)binary_tree_height_b(tree->right));
+		total = left - right;
+	}
+	return (total);
+}
