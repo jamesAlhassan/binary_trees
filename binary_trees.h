3bcb61d47435e29a93b98d7e39baa35eb9be8c50
James Alhassan <jamesalhassan052@gmail.com>
2023-03-03 14:32:48 +0000

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -1,14 +1,9 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
-#include <stdlib.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <math.h>
-#include <limits.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -18,21 +13,15 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-};
-typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s heap_t;
+} binary_tree_t;
 
-/* binary_tree_print.c */
 void binary_tree_print(const binary_tree_t *);
-/* tasks */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -48,8 +37,11 @@ size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
+size_t binary_tree_height_2(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
-int binary_tree_is_perfect(const binary_tree_t *tree)
+int binary_tree_is_perfect(const binary_tree_t *tree);
+int _pow_recursion(int x, int y);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-#endif
+
+#endif /* _BINARY_TREES_H_ */
