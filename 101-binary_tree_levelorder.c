4b650fd2afb48c414f880d398b92eb00f4910abb
James Alhassan <jamesalhassan052@gmail.com>
2023-03-04 05:08:52 +0000

    Update 101-binary_tree_levelorder.c

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -17,3 +17,19 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 	for (level = 1; level <= maxlevel; level++)
 		btlo_helper(tree, func, level);
 }
+/**
+ * btlo_helper - goes through a binary tree using post-order traverse
+ * @tree: tree to traverse
+ * @func: pointer to a function to call for each node
+ * @level: the level of the tree to call func upon
+ */
+void btlo_helper(const binary_tree_t *tree, void (*func)(int), size_t level)
+{
+	if (level == 1)
+		func(tree->n);
+	else
+	{
+		btlo_helper(tree->left, func, level - 1);
+		btlo_helper(tree->right, func, level - 1);
+	}
+}
