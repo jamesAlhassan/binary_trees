a26a98622a65e76e268885d5b8c1df81591d5869
James Alhassan <jamesalhassan052@gmail.com>
2023-03-09 05:53:28 +0000

    Update 114-bst_remove.c

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -24,3 +24,19 @@ int successor(bst_t *node)
 		return (left);
 	}
 }
+
+/**
+ * two_children - function that gets the next successor using the min
+ * value in the right subtree, and then replace the node value for
+ * this successor
+ * @root: node tat has two children
+ * Return: the value found
+ */
+int two_children(bst_t *root)
+{
+	int new_value = 0;
+
+	new_value = successor(root->right);
+	root->n = new_value;
+	return (new_value);
+}
