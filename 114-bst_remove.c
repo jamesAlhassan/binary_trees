c69e03739391b0878fe2e72442f350aaa729d0ea
James Alhassan <jamesalhassan052@gmail.com>
2023-03-09 05:54:10 +0000

    Update 114-bst_remove.c

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -81,3 +81,30 @@ int remove_type(bst_t *root)
 	else
 		return (two_children(root));
 }
+
+/**
+ * bst_remove - remove a node from a BST tree
+ * @root: root of the tree
+ * @value: node with this value to remove
+ * Return: the tree changed
+ */
+bst_t *bst_remove(bst_t *root, int value)
+{
+	int type = 0;
+
+	if (root == NULL)
+		return (NULL);
+	if (value < root->n)
+		bst_remove(root->left, value);
+	else if (value > root->n)
+		bst_remove(root->right, value);
+	else if (value == root->n)
+	{
+		type = remove_type(root);
+		if (type != 0)
+			bst_remove(root->right, type);
+	}
+	else
+		return (NULL);
+	return (root);
+}
